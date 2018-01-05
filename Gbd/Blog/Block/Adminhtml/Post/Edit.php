eaf8a53c4400e36fb78201ee14d124e0d7478a52
Prajwal Shrestha <prajwal_stha@yahoo.com>
2018-01-05 08:25:33 +0545

    Remove merge conflicts

---
+++ Gbd/Blog/Block/Adminhtml/Post/Edit.php
@@ -0,0 +1,80 @@
+<?php
+
+namespace Gbd\Blog\Block\Adminhtml\Post;
+
+/**
+ * Class Edit
+ * @package Mageplaza\Blog\Block\Adminhtml\Post
+ */
+class Edit extends \Magento\Backend\Block\Widget\Form\Container
+{
+	/**
+	 * Core registry
+	 *
+	 * @var \Magento\Framework\Registry
+	 */
+	public $coreRegistry;
+
+	/**
+	 * constructor
+	 *
+	 * @param \Magento\Framework\Registry $coreRegistry
+	 * @param \Magento\Backend\Block\Widget\Context $context
+	 * @param array $data
+	 */
+	public function __construct(
+		\Magento\Framework\Registry $coreRegistry,
+		\Magento\Backend\Block\Widget\Context $context,
+		array $data = []
+	)
+	{
+		$this->coreRegistry = $coreRegistry;
+		parent::__construct($context, $data);
+	}
+
+	/**
+	 * Initialize Post edit block
+	 *
+	 * @return void
+	 */
+	protected function _construct()
+	{
+		$this->_blockGroup = 'Gbd_Blog';
+		$this->_controller = 'adminhtml_post';
+
+		parent::_construct();
+
+		$this->buttonList->add(
+			'save-and-continue',
+			[
+				'label'          => __('Save and Continue Edit'),
+				'class'          => 'save',
+				'data_attribute' => [
+					'mage-init' => [
+						'button' => [
+							'event'  => 'saveAndContinueEdit',
+							'target' => '#edit_form'
+						]
+					]
+				]
+			],
+			-100
+		);
+	}
+
+	/**
+	 * Retrieve text for header element depending on loaded Post
+	 *
+	 * @return string
+	 */
+	public function getHeaderText()
+	{
+		/** @var \Mageplaza\Blog\Model\Post $post */
+		$post = $this->coreRegistry->registry('gbd_blog_post');
+		if ($post->getId()) {
+			return __("Edit Post '%1'", $this->escapeHtml($post->getName()));
+		}
+
+		return __('New Post');
+	}
+}
---
+++ Gbd/Blog/Controller/Adminhtml/Post/Edit.php
@@ -0,0 +1,74 @@
+<?php
+
+namespace Gbd\Blog\Controller\Adminhtml\Post;
+
+use Magento\Backend\App\Action\Context;
+use Magento\Framework\Registry;
+use Magento\Framework\View\Result\PageFactory;
+use Gbd\Blog\Controller\Adminhtml\Post;
+use Gbd\Blog\Model\PostFactory;
+
+/**
+ * Class Edit
+ * @package Mageplaza\Blog\Controller\Adminhtml\Post
+ */
+class Edit extends Post
+{
+	/**
+	 * Page factory
+	 *
+	 * @var \Magento\Framework\View\Result\PageFactory
+	 */
+	public $resultPageFactory;
+
+	/**
+	 * Edit constructor.
+	 * @param \Magento\Backend\App\Action\Context $context
+	 * @param \Magento\Framework\Registry $registry
+	 * @param \Gbd\Blog\Model\PostFactory $postFactory
+	 * @param \Magento\Framework\View\Result\PageFactory $resultPageFactory
+	 */
+	public function __construct(
+		Context $context,
+		Registry $registry,
+		PostFactory $postFactory,
+		PageFactory $resultPageFactory
+	)
+	{
+		$this->resultPageFactory = $resultPageFactory;
+
+		parent::__construct($postFactory, $registry, $context);
+	}
+
+	/**
+	 * @return \Magento\Backend\Model\View\Result\Page|\Magento\Framework\Controller\Result\Redirect|\Magento\Framework\View\Result\Page
+	 */
+	public function execute()
+	{
+		/** @var \Gbd\Blog\Model\Post $post */
+		$post = $this->initPost();
+		if (!$post) {
+			$resultRedirect = $this->resultRedirectFactory->create();
+			$resultRedirect->setPath('*');
+
+			return $resultRedirect;
+		}
+
+		$data = $this->_session->getData('gbd_blog_post_data', true);
+		if (!empty($data)) {
+			$post->setData($data);
+		}
+
+		$this->coreRegistry->register('gbd_blog_post', $post);
+
+		/** @var \Magento\Backend\Model\View\Result\Page|\Magento\Framework\View\Result\Page $resultPage */
+		$resultPage = $this->resultPageFactory->create();
+		$resultPage->setActiveMenu('Gbd_Blog::post');
+		$resultPage->getConfig()->getTitle()->set(__('Posts'));
+
+		$title = $post->getId() ? $post->getName() : __('New Post');
+		$resultPage->getConfig()->getTitle()->prepend($title);
+
+		return $resultPage;
+	}
+}
